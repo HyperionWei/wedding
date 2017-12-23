ec964b30db20c5a9a8a0c8b54daeee7aafb03d7f
HyperionWei <weixie@WEIs-MacBook-Pro.local>
2017-12-23 11:11:50 -0500

    move to git

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var fs = require("fs");
-var host = "127.0.0.1";
-var port = 1337;
-var express = require("express");
-
+var express = require('express');
+var path = require('path');
 var app = express();
-app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
 
-app.get("/", function(request, response){ //root dir
-    response.send("Hello!!");
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
 });
-
-app.listen(port, host);
